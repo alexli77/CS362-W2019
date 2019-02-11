8f69fb114b79d209eb073caefd9b264a61d946a6
alexli77 <38200963+alexli77@users.noreply.github.com>
2019-02-10 23:49:42 -0800

    Update unittest4.c

--- projects/lihua/dominion/unittest4.c
+++ projects/lihua/dominion/unittest4.c
@@ -18,18 +18,22 @@ int main()
 
     a = buyCard(10, &state);
 
-    ASSERT(a == -1," no buys.");
+   int assert1= ASSERT(a == -1," no buys.");
 
     state.numBuys = 10;
 
     state.coins = 0;
 
-    ASSERT(s == -1," not enough coins.");
+   int assert2= ASSERT(s == -1," not enough coins.");
 
     state.coins = 50;
 
-    ASSERT(s == 0,"enough buys and coins.");
+    int assert3=ASSERT(s == 0,"enough buys and coins.");
 
- 
+
+    if(assert1 +assert2+assert3== 0)
+     printf("TEST SUCCESSFULLY");
+  else
+     printf("TEST FAILED\n");
     return 0;
 } 
